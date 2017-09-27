b0b35d0977b0217172b405bfc31d477dab37bdb6
Nil Késede <nil@ksde.pw>
2017-09-27 09:56:25 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'xo-react/space',
+    'xo-space/esnext',
+    'plugin:unicorn/recommended'
+  ],
+  plugins: [
+    'unicorn'
+  ],
+  env: {
+    browser: true
+  }
+};

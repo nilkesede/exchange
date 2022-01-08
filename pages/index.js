c0184a5ec53a82ea5dadb0afa2434fbd3915675c
Nil Késede <nilkesede@gmail.com>
2022-01-08 10:15:15 -0300

    update packages and fixed api

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import money from 'money';
 
-const fetchRates = () => axios.get('https://api.fixer.io/latest').then(({data}) => data);
+const fetchRates = () => axios.get('http://data.fixer.io/api/latest?access_key=f8d17fe788bc2fbddb01bbfb42bc50bb').then(({data}) => data);
 
 class Index extends Component {
   static async getInitialProps() {
@@ -18,7 +18,7 @@ class Index extends Component {
     const value = 10;
     const converted = money.convert(value, {from: 'USD', to: 'BRL'});
 
-    return `${value.toFixed(2)} usd is ${converted.toFixed(2)} brl`;
+    return `${value.toFixed(2)} USD is ${converted.toFixed(2)} BRL`;
   }
 }
 
